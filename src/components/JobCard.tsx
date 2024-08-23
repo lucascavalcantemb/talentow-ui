e5302b5ac5108fc957290d3657b97ecd0387202f
Lucas Cavalcante <lucas.cavalcante.dev@icloud.com>
2024-08-23 20:08:44 -0300

    feat: add JobCard component

---
+++ src/components/JobCard.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { CircleDollarSign, Globe, Watch } from "lucide-react";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/Avatar";
+import { Button } from "@/components/Button";
+import { Badge } from "@/components/Badge";
+import { formatInitials } from "@/utils/format-initials";
+import { cn } from "@/lib/utils";
+
+export interface IJobCard {
+  className?: string;
+  jobOffer: {
+    title: string;
+    modality: string;
+    seniority: string;
+    salary: string;
+    contractType: string;
+    startDate: string;
+    company: {
+      name: string;
+      logo?: string;
+    };
+  };
+  textApply: string
+}
+
+const JobCard = ({ className, jobOffer, textApply }: IJobCard) => {
+  return (
+    <div className={cn('flex w-full cursor-pointer items-center justify-between rounded-md border-2 bg-card px-5 py-3 hover:bg-muted/50 hover:no-underline data-[state=open]:rounded-b-none data-[state=open]:bg-muted', className)}>
+      <div className="flex flex-col items-start justify-start gap-2">
+        <div className="flex items-center justify-center gap-3">
+          <Avatar className="size-16 border-2 border-muted-foreground">
+            <AvatarImage src={jobOffer.company.logo} />
+            <AvatarFallback className="font-bold">{formatInitials(jobOffer.company.name)}</AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col items-start justify-start">
+            <span>{jobOffer.company.name}</span>
+            <span className="text-lg font-bold">{jobOffer.title}</span>
+          </div>
+        </div>
+        <div className="flex items-center justify-start gap-2">
+          <Badge variant="outline">
+            <Globe className="mr-1 size-4 stroke-primary" />
+            <span className="text-xs">
+              {jobOffer.modality}
+            </span>
+          </Badge>
+          <Badge variant="outline">
+            <span className="text-xs">
+              {jobOffer.seniority}
+            </span>
+          </Badge>
+          <Badge variant="outline">
+            <CircleDollarSign className="mr-1 size-4 stroke-yellow-500" />
+            <span className="text-xs">{jobOffer.salary}</span>
+          </Badge>
+          <Badge variant="outline">
+            <span className="text-xs">
+              {jobOffer.contractType}
+            </span>
+          </Badge>
+          <Badge variant="outline">
+            <Watch className="mr-1 size-4 stroke-primary" />
+            <span className="text-xs">
+              {jobOffer.startDate}
+            </span>
+          </Badge>
+        </div>
+      </div>
+
+      <Button>{textApply}</Button>
+    </div>
+  );
+};
+JobCard.displayName = 'JobCard';
+
+export { JobCard }
---
+++ src/stories/JobCard.stories.tsx
@@ -0,0 +1,50 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { JobCard } from '@/components/JobCard';
+import { Accordion, AccordionItem, AccordionTrigger } from '@radix-ui/react-accordion';
+
+const meta = {
+  title: 'Components/JobCard',
+  component: JobCard,
+  args: {
+    textApply: 'Apply',
+    jobOffer: {
+      company: {
+        logo: '',
+        name: 'MB Labs',
+      },
+      contractType: 'PJ',
+      modality: 'Remote',
+      seniority: 'Senior',
+      salary: '$ 5k',
+      startDate: '2 weeks ago',
+      title: 'Developer Node JS - Senior',
+    }
+  },
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: 'The `JobCard` component needs to be wrapped by `AccordionTrigger`, and the `asChild` prop should be set to `true` in the `AccordionTrigger`.',
+      },
+    },
+  },
+} satisfies Meta<typeof JobCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: (args) => (
+    <div className='w-[768px]'>
+      <Accordion type='single'>
+        <AccordionItem value="item-1" className='mb-2'>
+          <AccordionTrigger className='w-full'>
+            <JobCard {...args} />
+          </AccordionTrigger>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  )
+};
+
